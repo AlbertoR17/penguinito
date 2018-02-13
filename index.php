03c28b118b893daffcf176dca49b24e75e51a63b
AlbertoR17 <alberto9406@gmail.com>
2018-02-13 11:00:32 -0700

    cambio de extension
    y la imagen

--- index.php
+++ index.php
@@ -12,8 +12,18 @@
 </head>
 
 <body>
-	<!--Import jQuery before materialize.js-->
+ <nav>
+    <div class="nav-wrapper">
+      <a href="#!" class="brand-logo"><img src="img/penguin.png" alt="" style="width: 60px;height: 60px"></a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="sass.html"><i class="material-icons left">search</i>Link with Left Icon</a></li>
+        <li><a href="badges.html"><i class="material-icons right">view_module</i>Link with Right Icon</a></li>
+      </ul>
+    </div>
+  </nav>
+	
+</body>
+<!--Import jQuery before materialize.js-->
 	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
 	<script type="text/javascript" src="js/materialize.min.js"></script>
-</body>
 </html>
