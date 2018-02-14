ad641f5e032cca392f031ecf520f00f01f8cbe3f
AlbertoR17 <alberto9406@gmail.com>
2018-02-14 13:30:37 -0700

    public
    publica

---
+++ publica.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!--Import Google Icon Font-->
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+	<!--Import materialize.css-->
+	<link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+
+	<!--Let browser know website is optimized for mobile-->
+	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+	<title>penguinito</title>
+</head>
+
+<body>
+ <nav>
+    <div class="nav-wrapper">
+      <a href="index.php" class="brand-logo"><img src="img/penguin.png" alt="" style="width: 60px;height: 60px"><span style="font-family: medium italic">Penguinito</span></a>
+      <ul class="right hide-on-med-and-down">
+      	<li><a href="index.php"><i class="material-icons left">fiber_new</i>Nuevo Post</a></li>
+      	<li><a href="index.php"><i class="material-icons left">view_list</i>Historial</a></li>
+        <li><a href="index.php"><i class="material-icons left">exit_to_app</i>Salir</a></li>
+      </ul>
+    </div>
+  </nav>
+	<!-- inico de contenido -->
+	<center>
+	<h3>[Titulo]</h3>
+	<h5>[Subtitulo]</h5>
+	</center>
+	<div class="container">
+		<div class="row">
+			<div class="col m2">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut unde, saepe illum culpa possimus nobis dignissimos mollitia odio, non ad. In ullam magnam ab autem, officia! Accusantium nobis, tenetur ipsum.
+			</div>
+			<div class="col m6">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque, velit consequuntur iure, aliquid ab deserunt, sequi obcaecati veniam at officia illo quaerat? Ad eius placeat, accusamus deserunt error dicta?
+			</div>
+			<div class="col m2">
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, fuga enim consectetur repellendus! Rem, sit, distinctio tenetur incidunt iste ab fugiat, consectetur perspiciatis omnis accusantium quia illo est reiciendis nihil.
+			</div>
+
+
+		</div>
+	</div>
+	<!-- fin de contenido -->
+
+</body>
+<!--Import jQuery before materialize.js-->
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="js/materialize.min.js"></script>
+</html>
